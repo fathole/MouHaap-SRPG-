5d95ec7098e0cd3ca7ef1f13425d77e309bdd4bf
TW <tomwonglokho@gmail.com>
2023-05-10 19:54:43 +0800

    1.0.0
    Remove unused scene and plugins.

--- Assets/Scripts/GameManager/Manager/ControllerManager/CameraManager.cs
+++ Assets/Scripts/GameManager/Manager/ControllerManager/CameraManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.Rendering.Universal;
 using Cinemachine;
 

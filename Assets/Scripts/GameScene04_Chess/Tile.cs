86439c76238808d8e5e2cb4e79e3621fa3974660
TW <tomwonglokho@gmail.com>
2023-05-16 18:42:27 +0800

    1.0.0

--- Assets/Scripts/GameScene04_Chess/Tile.cs
+++ Assets/Scripts/GameScene04_Chess/Tile.cs
@@ -53,13 +53,4 @@ namespace ChessScene
 
         #endregion
     }
-
-    public enum TileNoticeOption
-    {
-        None = 0,
-        Frontier = 1,// The Path From Current Chess To Target Tile
-        Current = 2,// The Tail Of Current Chess
-        Valid = 3,// The Tail That Current Chess Can Reach
-        Attack = 4,// The Tile That Current Chess Can Attack
-    }
 }

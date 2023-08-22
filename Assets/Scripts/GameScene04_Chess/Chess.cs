d248f905b9a4c537bd99466193f70a3dbfffbe18
TW <tomwonglokho@gmail.com>
2023-08-22 23:26:11 +0800

    1.0.0

--- Assets/Scripts/GameScene04_Chess/Chess.cs
+++ Assets/Scripts/GameScene04_Chess/Chess.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Chess : MonoBehaviour
-{    
+{
+    #region Declaration _ Variable
+
+    #endregion
+
+    #region Init Stage
+
     public void InitChess()
     {
 
     }
 
+    #endregion
+
+    #region Setup Stage
+
     public void SetupChess()
     {
 
     }
+
+    #endregion    
 }

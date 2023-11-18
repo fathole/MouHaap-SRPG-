445cf9c246d43d9f09c5a15fb8f0af5a95a0b1f1
TW <tomwonglokho@gmail.com>
2023-11-18 14:00:12 +0800

    1.0.0

--- Assets/Scripts/GameManager/DataClass/SceneOptionData.cs
+++
@@ -1,8 +0,0 @@
-﻿public enum SceneOption
-{
-    None = 0,
-    GameScene01_Start = 1,
-    GameScene02_Home = 2,
-    GameScene03_World = 3,
-    GameScene04_Chess = 4,
-}
--- Assets/Scripts/GameManager/DataClass/SortingLayerOptionData.cs
+++
@@ -1,9 +0,0 @@
-public enum SortingLayerOption
-{
-    Default = 0,
-    World = 1,
-    UI_Main = 2,
-    UI_AppMain = 3,
-    UI_Popup = 4,
-    UI_AppPopup = 5,    
-}

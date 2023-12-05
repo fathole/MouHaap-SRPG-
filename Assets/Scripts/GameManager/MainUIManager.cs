714c906ed7bc17e7f4dc70c99d2a458b571b7a40
TW <tomwonglokho@gmail.com>
2023-12-05 21:21:05 +0800

    1.0.0

--- Assets/Scripts/GameManager/MainUIManager.cs
+++ Assets/Scripts/GameManager/MainUIManager.cs
@@ -15,6 +15,9 @@ public class MainUIManager : SceneManagerBase
 
     [Header("Panel")]
     public ExitPanel exitPanel;
+    public StoryModePanel storyModePanel;
+    public ChallengeModePanel challengeModePanel;
+    public RealisticModePanel realisticModePanel;
 
     #endregion
 
@@ -89,16 +92,22 @@ public class MainUIManager : SceneManagerBase
     private void HomePage_O_StoryModeButtonPointerClickCallback()
     {
         Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(MoveInStoryModePanel());
     }
 
     private void HomePage_O_ChallengeModeButtonPointerClickCallback()
     {
         Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(MoveInChallengeModePanel());
     }
 
     private void HomePage_O_RealisticModeButtonPointerClickCallback()
     {
         Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(MoveInRealisticModePanel());
     }
 
     private void HomePage_O_GameModeBackButtonPointerClickCallback()
@@ -140,6 +149,97 @@ public class MainUIManager : SceneManagerBase
         Debug.Log(MethodBase.GetCurrentMethod().Name);
 
         StartCoroutine(exitPanel.FadeOutCoroutine());
+    }
+
+    #endregion
+
+    #region Function - StoryModePanel
+
+    private IEnumerator MoveInStoryModePanel()
+    {
+        // Init Panel
+        storyModePanel.InitPanel();
+
+        // Init Panel Object
+        storyModePanel.o_CrossButton.InitObject(StoryModePanel_O_CrossButtonPointerClickCallback);
+        storyModePanel.o_TickButton.InitObject(StoryModePanel_O_TickButtonPointerClickCallback);
+
+        yield return storyModePanel.FadeInCoroutine();
+    }
+
+    private void StoryModePanel_O_TickButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        // ToDo: 
+    }
+
+    private void StoryModePanel_O_CrossButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(storyModePanel.FadeOutCoroutine());
+    }
+
+    #endregion
+
+    #region Function - ChallengeModePanel
+
+    private IEnumerator MoveInChallengeModePanel()
+    {
+        // Init Panel
+        challengeModePanel.InitPanel();
+
+        // Init Panel Object
+        challengeModePanel.o_CrossButton.InitObject(ChallengeModePanel_O_CrossButtonPointerClickCallback);
+        challengeModePanel.o_TickButton.InitObject(ChallengeModePanel_O_TickButtonPointerClickCallback);
+
+        yield return challengeModePanel.FadeInCoroutine();
+
+    }
+
+    private void ChallengeModePanel_O_TickButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        // ToDo: 
+    }
+
+    private void ChallengeModePanel_O_CrossButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(challengeModePanel.FadeOutCoroutine());
+    }
+
+    #endregion
+
+    #region Function - RealisticModePanel
+
+    private IEnumerator MoveInRealisticModePanel()
+    {
+        // Init Panel
+        realisticModePanel.InitPanel();
+
+        // Init Panel Object
+        realisticModePanel.o_CrossButton.InitObject(RealisticModePanel_O_CrossButtonPointerClickCallback);
+        realisticModePanel.o_TickButton.InitObject(RealisticModePanel_O_TickButtonPointerClickCallback);
+
+        yield return realisticModePanel.FadeInCoroutine();
+    }
+
+    private void RealisticModePanel_O_TickButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        // ToDo: 
+    }
+
+    private void RealisticModePanel_O_CrossButtonPointerClickCallback()
+    {
+        Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+        StartCoroutine(realisticModePanel.FadeOutCoroutine());
     }
 
     #endregion

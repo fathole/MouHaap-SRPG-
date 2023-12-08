7d99453147ac7be3317afe0efa4cadf097e3c862
TW <tomwonglokho@gmail.com>
2023-12-08 22:55:02 +0800

    1.0.0

---
+++ Assets/Scripts/Object/MainUI/StoryModePanel/StoryModePanel.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryModePanel : PanelBase
+{
+    [Header("Object")]
+    public Common_Button o_CrossButton;
+    public Common_Button o_TickButton;
+}

10c5a81c625d53e0b8f2d16ca16247578c9e9d6b
TW <tomwonglokho@gmail.com>
2024-03-26 22:24:29 +0800

    1.0.0
    Updated Group Handle And Difficulty Page

--- Assets/Scripts/MainUI/Manager/MainUIManager.cs
+++ Assets/Scripts/MainUI/Manager/MainUIManager.cs
@@ -13,11 +13,22 @@ namespace MainUI
         public static MainUIManager instance;        
 
         [Header("Object")]
+        // MainMenu
         public Common_Button o_CreditsButton;
         public Common_Button o_LoadGameButton;
         public Common_Button o_NewGameButton;
         public Common_Button o_QuitGameButton;
         public Common_Button o_SettingButton;
+        // Difficulty
+        public Common_Button o_DifficultyBackButton;
+        public Common_Button o_ExploreButton;        
+        public Common_Button O_NormalButton;        
+        public Common_Button O_HardButton;        
+        public Common_Button O_HellButton;
+
+        [Header("Group")]
+        public GameObject g_MainMenu;
+        public GameObject g_Difficulty;
 
         [Header("Popup")]
         public Popup_QuitGame popup_QuitGame;
@@ -47,12 +58,22 @@ namespace MainUI
         }
 
         private void InitElement()
-        {            
+        {
+            #region Init - Object
+
             o_CreditsButton.InitObject(O_CreditsButtonOnPointerClickCallback);
             o_LoadGameButton.InitObject(O_LoadGameButtonOnPointerClickCallback);
             o_NewGameButton.InitObject(O_NewGameButtonOnPointerClickCallback);
             o_QuitGameButton.InitObject(O_QuitGameButtonOnPointerClickCallback);
             o_SettingButton.InitObject(O_SettingButtonOnPointerClickCallback);
+
+            o_DifficultyBackButton.InitObject(O_DifficultyBackButtonOnPointerClickCallback);
+            o_ExploreButton.InitObject(o_ExploreButtonOnPointerClickCallback);
+            O_NormalButton.InitObject(O_NormalButtonOnPointerClickCallback);
+            O_HardButton.InitObject(O_HardButtonOnPointerClickCallback);
+            O_HellButton.InitObject(O_HellButtonOnPointerClickCallback);
+
+            #endregion
 
             #region Init - Popup_QuitGame
 
@@ -68,6 +89,7 @@ namespace MainUI
 
         #region Function - Object
 
+        // Main Menu
         public void O_CreditsButtonOnPointerClickCallback()
         {
             Debug.Log(MethodBase.GetCurrentMethod().Name);
@@ -83,6 +105,9 @@ namespace MainUI
         public void O_NewGameButtonOnPointerClickCallback()
         {
             Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+            g_MainMenu.gameObject.SetActive(false);
+            g_Difficulty.gameObject.SetActive(true);            
         }
 
         public void O_QuitGameButtonOnPointerClickCallback()
@@ -93,6 +118,35 @@ namespace MainUI
         }
 
         public void O_SettingButtonOnPointerClickCallback()
+        {
+            Debug.Log(MethodBase.GetCurrentMethod().Name);
+        }
+
+        // Difficulty
+        public void O_DifficultyBackButtonOnPointerClickCallback()
+        {
+            Debug.Log(MethodBase.GetCurrentMethod().Name);
+
+            g_MainMenu.gameObject.SetActive(true);
+            g_Difficulty.gameObject.SetActive(false);            
+        }
+
+        public void o_ExploreButtonOnPointerClickCallback()
+        {
+            Debug.Log(MethodBase.GetCurrentMethod().Name);
+        }
+
+        public void O_NormalButtonOnPointerClickCallback()
+        {
+            Debug.Log(MethodBase.GetCurrentMethod().Name);
+        }
+
+        public void O_HardButtonOnPointerClickCallback()
+        {
+            Debug.Log(MethodBase.GetCurrentMethod().Name);
+        }
+
+        public void O_HellButtonOnPointerClickCallback()
         {
             Debug.Log(MethodBase.GetCurrentMethod().Name);
         }
